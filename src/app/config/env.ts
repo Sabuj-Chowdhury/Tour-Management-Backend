a579e5acb79b89affaa1c432a2b239067a4b0aab
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-07-07 10:17:23 +0530

    env configure

---
+++ src/app/config/env.ts
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+interface IEnv {
+  PORT: string;
+  DB_URL: string;
+  NODE_ENV: "development" | "production";
+}
+
+const requiredVariable = ["PORT", "DB_URL", "NODE_ENV"];
+const loadEnv = (): IEnv => {
+  requiredVariable.forEach((key) => {
+    if (!process.env[key]) {
+      throw new Error(`Missing required env variables ${key}`);
+    }
+  });
+  return {
+    PORT: process.env.PORT as string,
+    DB_URL: process.env.DB_URL!,
+    NODE_ENV: process.env.NODE_ENV as "development" | "production",
+  };
+};
+
+export const envVariable = loadEnv();
