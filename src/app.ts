e704c454f7641927bd09bacf8045249a1a304408
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-07-08 19:35:01 +0530

    Create User and organizing Routing System

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
-import { userRouter } from "./app/modules/user/user.route";
+
 import cors from "cors";
+import { router } from "./app/routes";
 const app = express();
 
 // middleware
@@ -8,7 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 // routes
-app.use("/api/v1/user", userRouter);
+app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
