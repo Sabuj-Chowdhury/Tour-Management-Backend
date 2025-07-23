183c5e7c7fcf6d284b729ef5f2a977da64d822de
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-07-23 15:16:03 +0600

    Split User Controller and Make Service Layer

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,15 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from "express";
-import { User } from "./user.model";
+
 import httpStats from "http-status-codes";
+import { userServices } from "./user.services";
 
 const createUser = async (req: Request, res: Response) => {
   try {
-    const { name, email } = req.body;
-    const user = await User.create({
-      name,
-      email,
-    });
+    /*
+    ***this moved to the user Service layer
+    // const { name, email } = req.body;
+    // const user = await User.create({
+    //   name,
+    //   email,
+    // });
+    */
+
+    // this is where service layer is used
+    const user = await userServices.createUser(req.body);
     res.status(httpStats.CREATED).json({
       message: "User created successfully.",
       user,
