27cbeea36738520ccb6050f864157eee5b3e3708
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-07-24 09:26:56 +0600

    using tryCatch utill in user controller

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,31 +1,48 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from "express";
 
 import httpStats from "http-status-codes";
 import { userServices } from "./user.services";
+import tryCatch from "../../utils/tryCatch";
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    /*
-    ***this moved to the user Service layer
-    // const { name, email } = req.body;
-    // const user = await User.create({
-    //   name,
-    //   email,
-    // });
-    */
+// before using tryCatch util function
 
-    // this is where service layer is used
+// const createUser = async (req: Request, res: Response, next: NextFunction) => {
+//   try {
+//     /*
+//     ***this moved to the user Service layer
+//     // const { name, email } = req.body;
+//     // const user = await User.create({
+//     //   name,
+//     //   email,
+//     // });
+//     */
+
+//     // this is where service layer is used
+//     const user = await userServices.createUser(req.body);
+//     res.status(httpStats.CREATED).json({
+//       message: "User created successfully.",
+//       user,
+//     });
+//   } catch (err: any) {
+//     console.log(err);
+//     next(err);
+//   }
+// };
+
+// after using tryCatch util function
+
+const createUser = tryCatch(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async (req: Request, res: Response, next: NextFunction) => {
     const user = await userServices.createUser(req.body);
+
     res.status(httpStats.CREATED).json({
-      message: "User created successfully.",
+      success: true,
+      message: "User Created Successfully!",
       user,
     });
-  } catch (err: any) {
-    console.log(err);
-    next(err);
   }
-};
+);
 
 export const userController = {
   createUser,
