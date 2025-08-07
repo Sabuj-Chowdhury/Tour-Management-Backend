816fe5c96795c91418b260f3a6d42b66c3e61f62
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-08-08 04:02:58 +0600

    Verify Token and protect route using middleware

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,7 +1,11 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { userController } from "./user.controller";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { userZodSchema } from "./user.validation";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import AppError from "../../errorHelpers/AppError";
+import httpStatus from "http-status-codes";
+import { Role } from "./user.interface";
 
 export const userRouter = Router();
 
@@ -10,4 +14,31 @@ userRouter.post(
   validateRequest(userZodSchema),
   userController.createUser
 );
-userRouter.get("/all-users", userController.getAllUsers);
+userRouter.get(
+  "/all-users",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const accessToken = req.headers.authorization;
+      if (!accessToken) {
+        throw new AppError(httpStatus.BAD_REQUEST, "No token Received!");
+      }
+
+      const verifyToken = jwt.verify(accessToken, "secret");
+
+      if (!verifyToken) {
+        throw new AppError(httpStatus.BAD_REQUEST, "You are not authorized!");
+      }
+      if ((verifyToken as JwtPayload).role !== Role.ADMIN) {
+        throw new AppError(
+          httpStatus.BAD_REQUEST,
+          "You are not permitted to view this Route!"
+        );
+      }
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  },
+  userController.getAllUsers
+);
