bfebff2df384947db0513cca6ee20475e0bcea5c
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-08-11 19:20:30 +0600

    Set the accessToken and refreshToken in Cookies

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response } from "express";
-
 import cors from "cors";
 import { router } from "./app/routes";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
+import cookieParser from "cookie-parser";
+
 const app = express();
 
 // middleware
+app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
 
--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -3,11 +3,22 @@ import tryCatch from "../../utils/tryCatch";
 import { sendResponse } from "../../utils/sendResponse";
 import httpStatus from "http-status-codes";
 import { authServices } from "./auth.service";
+import AppError from "../../errorHelpers/AppError";
 
 const credentialLogin = tryCatch(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async (req: Request, res: Response, next: NextFunction) => {
     const loginInfo = await authServices.credentialLogin(req.body);
+
+    res.cookie("accessToken", loginInfo.accessToken, {
+      httpOnly: true,
+      secure: false,
+    });
+
+    res.cookie("refreshToken", loginInfo.refreshToken, {
+      httpOnly: true,
+      secure: false,
+    });
 
     sendResponse(res, {
       success: true,
@@ -22,11 +33,17 @@ const newAccessToken = tryCatch(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async (req: Request, res: Response, next: NextFunction) => {
     const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+      throw new AppError(
+        httpStatus.BAD_REQUEST,
+        "Invalid or no refresh token!"
+      );
+    }
     const tokenInfo = await authServices.getNewAccessToken(refreshToken);
     sendResponse(res, {
       success: true,
       statusCode: httpStatus.OK,
-      message: "",
+      message: "New access token set successfully!",
       data: tokenInfo,
     });
   }
