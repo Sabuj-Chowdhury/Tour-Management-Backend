7f1c07a088c665900af5a86145cb3689aa147a51
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-08-13 00:58:45 +0600

    SERVICE: create tour types

--- src/app/modules/tour/tour.service.ts
+++ src/app/modules/tour/tour.service.ts
@@ -1,7 +1,19 @@
 import AppError from "../../errorHelpers/AppError";
-import { ITour } from "./tour.interface";
-import { Tour } from "./tour.model";
+import { ITour, ITourType } from "./tour.interface";
+import { Tour, TourType } from "./tour.model";
 import httpStatus from "http-status-codes";
+
+const createTourTypes = async (payload: ITourType) => {
+  const existingTourTypes = await TourType.findOne({ name: payload.name });
+
+  if (existingTourTypes) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Tour type already exists.");
+  }
+
+  const tourTypes = await TourType.create(payload);
+
+  return tourTypes;
+};
 
 const createTour = async (payload: ITour) => {
   const existingTour = await Tour.findOne({ title: payload.title });
@@ -18,5 +30,6 @@ const createTour = async (payload: ITour) => {
 };
 
 export const tourService = {
+  createTourTypes,
   createTour,
 };
