12502da0ddadcc7ed7d960bfc71335bf0f1fbca7
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-08-12 23:46:44 +0600

    update tour and tour types zod schema validation

--- src/app/modules/tour/tour.validation.ts
+++ src/app/modules/tour/tour.validation.ts
@@ -18,3 +18,25 @@ export const createTourZodSchema = z.object({
   departureLocation: z.string().optional(),
   arrivalLocation: z.string().optional(),
 });
+
+export const updateTourZodSchema = z.object({
+  title: z.string().optional(),
+  description: z.string().optional(),
+  location: z.string().optional(),
+  costFrom: z.number().optional(),
+  startDate: z.string().optional().optional(),
+  endDate: z.string().optional().optional(),
+  tourType: z.string().optional(), // <- changed here
+  included: z.array(z.string()).optional(),
+  excluded: z.array(z.string()).optional(),
+  amenities: z.array(z.string()).optional(),
+  tourPlan: z.array(z.string()).optional(),
+  maxGuest: z.number().optional(),
+  minAge: z.number().optional(),
+  departureLocation: z.string().optional(),
+  arrivalLocation: z.string().optional(),
+});
+
+export const createTourTypeZodSchema = z.object({
+  name: z.string(),
+});
