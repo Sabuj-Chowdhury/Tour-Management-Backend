d5e010c805aac17621c5da135724243e81d442fa
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-08-24 06:37:03 +0600

    CONTROLLER : success payment to redirect frontend success url

---
+++ src/app/modules/payment/payment.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import tryCatch from "../../utils/tryCatch";
+import { paymentService } from "./payment.service";
+import { envVariable } from "../../config/env";
+
+const successPayment = tryCatch(async (req: Request, res: Response) => {
+  const query = req.query;
+  const result = await paymentService.successPayment(
+    query as Record<string, string>
+  );
+  if (result.success) {
+    res.redirect(envVariable.SSL.SSL_SUCCESS_FRONTEND_URL);
+  }
+});
+
+export const paymentController = {
+  successPayment,
+};
