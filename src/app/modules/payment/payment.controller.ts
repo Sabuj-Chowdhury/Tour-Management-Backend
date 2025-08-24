0a774f5e53dd0f027e1f0999dc7b69904451a646
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-08-24 07:22:53 +0600

    CONTROLLER : fail payment

--- src/app/modules/payment/payment.controller.ts
+++ src/app/modules/payment/payment.controller.ts
@@ -10,7 +10,7 @@ const successPayment = tryCatch(async (req: Request, res: Response) => {
   );
   if (result.success) {
     res.redirect(
-      `${envVariable.SSL.SSL_SUCCESS_FRONTEND_URL}?transactionId=${query.transactionId}&amount=${query.amount}&status=success&message=${result.message}`
+      `${envVariable.SSL.SSL_SUCCESS_FRONTEND_URL}?transactionId=${query.transactionId}&amount=${query.amount}&message=${result.message}&status=${query.status}`
     );
   }
 });
@@ -22,7 +22,19 @@ const cancelPayment = tryCatch(async (req: Request, res: Response) => {
   );
   if (!result.success) {
     res.redirect(
-      `${envVariable.SSL.SSL_CANCEL_FRONTEND_URL}?transactionId=${query.transactionId}&amount=${query.amount}&status=success&message=${result.message}`
+      `${envVariable.SSL.SSL_CANCEL_FRONTEND_URL}?transactionId=${query.transactionId}&amount=${query.amount}&message=${result.message}&status=${query.status}`
+    );
+  }
+});
+
+const failPayment = tryCatch(async (req: Request, res: Response) => {
+  const query = req.query;
+  const result = await paymentService.failPayment(
+    query as Record<string, string>
+  );
+  if (!result.success) {
+    res.redirect(
+      `${envVariable.SSL.SSL_FAIL_FRONTEND_URL}?transactionId=${query.transactionId}&amount=${query.amount}&message=${result.message}&status=${query.status}`
     );
   }
 });
@@ -30,4 +42,5 @@ const cancelPayment = tryCatch(async (req: Request, res: Response) => {
 export const paymentController = {
   successPayment,
   cancelPayment,
+  failPayment,
 };
