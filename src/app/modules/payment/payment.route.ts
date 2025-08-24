fbf2bc72dc4b416a59d993c356145270349d70db
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-08-24 06:40:42 +0600

    route success

--- src/app/modules/payment/payment.route.ts
+++ src/app/modules/payment/payment.route.ts
@@ -1,3 +1,6 @@
 import { Router } from "express";
+import { paymentController } from "./payment.controller";
 
 export const paymentRouter = Router();
+
+paymentRouter.post("/success", paymentController.successPayment);
