9cc6042b886dffb9fccaf16996468abaa55244c3
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-08-24 07:24:31 +0600

    cancel and fail payment api routes

--- src/app/modules/payment/payment.route.ts
+++ src/app/modules/payment/payment.route.ts
@@ -4,3 +4,5 @@ import { paymentController } from "./payment.controller";
 export const paymentRouter = Router();
 
 paymentRouter.post("/success", paymentController.successPayment);
+paymentRouter.post("/fail", paymentController.failPayment);
+paymentRouter.post("/cancel", paymentController.cancelPayment);
